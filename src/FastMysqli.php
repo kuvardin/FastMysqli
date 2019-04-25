42359ac22495fa11526f8a006605fa93f756755e
Maxim Kuvardin <kuvard.in@mail.ru>
2019-04-25 13:16:13 +0600

    Deleted _row from methods names

--- src/FastMysqli.php
+++ src/FastMysqli.php
@@ -6,7 +6,7 @@
  */
 class FastMysqli extends Mysqli
 {
-    public const NOT_NULL = 'IC&4j440*M&yl8X(H%41b%L)sA(ca0Z9&ab52YpX#16*1O%x';
+    public const NOT_NULL = 'IC&4j440Ï*M&yl8аÑX(H%41b%L)sA(ca0Z9&ab52YpX#16*1O%x';
 
     /**
      * @var int
@@ -21,7 +21,7 @@ class FastMysqli extends Mysqli
      * @return bool|mysqli_result
      * @throws Error
      */
-    public function fast_update_row(string $table, $row, $where = null, int $limit = null)
+    public function fast_update(string $table, $row, $where = null, int $limit = null)
     {
         $query_string = 'UPDATE `' . $this->filter($table) . '` SET ' . $this->fast_datalist_gen($row);
 
@@ -44,7 +44,7 @@ class FastMysqli extends Mysqli
      * @return bool|mysqli_result
      * @throws Error
      */
-    public function fast_delete_row(string $table, $where = null, int $limit = null)
+    public function fast_delete(string $table, $where = null, int $limit = null)
     {
         $query_string = 'DELETE FROM `' . $this->filter($table) . '`';
 
@@ -70,7 +70,7 @@ class FastMysqli extends Mysqli
      * @return bool|mysqli_result
      * @throws Error
      */
-    public function fast_select_row(string $table, $where = null, int $limit = null, string $ord = null, string $sort = null, int $offset = null)
+    public function fast_select(string $table, $where = null, int $limit = null, string $ord = null, string $sort = null, int $offset = null)
     {
         $query_string = 'SELECT * FROM `' . $this->filter($table) . '`';
 
@@ -97,7 +97,7 @@ class FastMysqli extends Mysqli
      * @return int
      * @throws Error
      */
-    public function fast_count_row(string $table, $where = null): int
+    public function fast_count(string $table, $where = null): int
     {
         $query_string = 'SELECT COUNT(*) FROM `' . $this->filter($table) . '`';
 
@@ -105,7 +105,8 @@ class FastMysqli extends Mysqli
             $query_string .= ' WHERE ' . $this->fast_where_gen($where);
         }
 
-        return (int)$this->q($query_string)->fetch_array()[0];
+        $response = $this->q($query_string);
+        return $response === false ? 0 : (int)$response->fetch_array()[0];
     }
 
 
@@ -115,7 +116,7 @@ class FastMysqli extends Mysqli
      * @return bool|mysqli_result
      * @throws Error
      */
-    public function fast_add_row(string $table, $row)
+    public function fast_add(string $table, $row)
     {
         $query_string = 'INSERT INTO `' . $this->filter($table) . '` SET ' . $this->fast_datalist_gen($row);
         return $this->q($query_string);
@@ -127,7 +128,7 @@ class FastMysqli extends Mysqli
      * @return bool
      * @throws Error
      */
-    public function fast_check_row(string $table, $where): bool
+    public function fast_check(string $table, $where): bool
     {
         $query_string = 'SELECT COUNT(*) FROM `' . $this->filter($table) . '` WHERE ' . $this->fast_where_gen($where) . ' LIMIT 1';
         return (bool)$this->q($query_string)->fetch_array()[0];
@@ -221,17 +222,17 @@ class FastMysqli extends Mysqli
     }
 
     /**
-     * @param string $query_text
+     * @param string $query
+     * @param int $resultmode
      * @return bool|mysqli_result
-     * @throws Error
      */
-    public function q(string $query_text)
+    public function q(string $query, int $resultmode = MYSQLI_STORE_RESULT)
     {
         $this->queries_counter++;
-        $result = $this->query($query_text);
+        $result = $this->query($query, $resultmode);
 
         if ($result === false) {
-            throw new Error("Mysql error \"{$this->error}\" in query \"{$query_text}\"");
+            throw new Error("Mysql error \"{$this->error}\" in query \"{$query}\"");
         }
 
         return $result;
@@ -255,7 +256,7 @@ class FastMysqli extends Mysqli
     public function fast_get_all_rows(string $table_name, string $index = null, $where = null): array
     {
         $result = [];
-        $rows = $this->fast_select_row($table_name, $where);
+        $rows = $this->fast_select($table_name, $where);
         while ($row = $rows->fetch_assoc()) {
             if ($index === null) {
                 $result[] = $row;
@@ -275,9 +276,9 @@ class FastMysqli extends Mysqli
      */
     public function fast_get_row_or_create(string $table_name, $identify_data, $adding_data = []): array
     {
-        while (!$row = $this->fast_select_row($table_name, $identify_data, 1)->fetch_assoc()) {
+        while (!$row = $this->fast_select($table_name, $identify_data, 1)->fetch_assoc()) {
             $row_data = is_array($identify_data) ? array_merge($adding_data, $identify_data) : $adding_data;
-            $this->fast_add_row($table_name, $row_data);
+            $this->fast_add($table_name, $row_data);
         }
         return $row;
     }
